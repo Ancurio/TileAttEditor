4a53bfa7eb7e109f23db2b715168d66a3d0b9f08
Jonas Kulla <Nyocurio@gmail.com>
2012-01-23 21:17:06 +0100

    Added zlib license notice to all files + separate COPYING file

--- color.h
+++ color.h
@@ -1,7 +1,40 @@
+/*
+ * color.h
+ *
+ * Copyright (c) 2011-2012 Jonas Kulla <Nyocurio@googlemail.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you must not
+ *  claim that you wrote the original software. If you use this software
+ *  in a product, an acknowledgment in the product documentation would be
+ *  appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *  misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution.
+ *
+ *
+ * WARNING: This is BAD code. Please do not try to learn from this.
+ *          You are, however, free to learn how NOT to write good software.
+ *          Have a nice day.
+ */
 
 
 
-/* Colors are bit-depth independent RGBA values in range [0;1] */
+
+/* Colors are bit-depth independent RGBA values in range [0;1]
+ * This is basically a re-implementation of GdkRGBA available in GDK3
+ */
+
 struct Color
 {
 	gdouble r;
--- ui.c
+++ ui.c
@@ -1,3 +1,33 @@
+/*
+ * ui.c
+ *
+ * Copyright (c) 2011-2012 Jonas Kulla <Nyocurio@googlemail.com>
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you must not
+ *  claim that you wrote the original software. If you use this software
+ *  in a product, an acknowledgment in the product documentation would be
+ *  appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such, and must not be
+ *  misrepresented as being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source
+ *  distribution.
+ *
+ *
+ * WARNING: This is BAD code. Please do not try to learn from this.
+ *          You are, however, free to learn how NOT to write good software.
+ *          Have a nice day.
+ */
+
 
 #include <gtk/gtk.h>
 
